d760464f45e60c34f605b80c756903f68087319a
O'Brian Santos <obrian.santos@gmail.com>
2022-08-18 22:24:06 -0700

    Added in pallete and initial components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [results, setResults] = useState([]);
+
+  const getSuggestions = (event) => {
+    console.log(event.target.value);
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="container flex flex-col mx-auto min-h-screen bg-lime-100">
+      <div className='flex m-6 flex-col h-48 justify-evenly items-center'>
+        <div className='text-lg text-lime-600 text-center font-semibold'> Look up your saved memes to send to friends!</div>
+        <input onChange={(e) => getSuggestions(e)} className='p-2 rounded-md w-48 h-6'></input>
+
+        {/* <button onClick={() => console.log(search)} className='text-lime-50 bg-lime-600 rounded-2xl w-36 h-8'>Search</button> */}
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    colors: {
+      "lime": {
+        "50": "#F4FCF3",
+        "100": "#E9F8E7",
+        "200": "#D3F2CF",
+        "300": "#BCEBB7",
+        "400": "#A2E39B",
+        "500": "#8DDD84",
+        "600": "#58CD4B",
+        "700": "#3BA82E",
+        "800": "#27701F",
+        "900": "#14380F"
+      }      
+    },
     extend: {},
   },
   plugins: [],
