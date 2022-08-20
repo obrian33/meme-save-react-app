bfa810c6c6da1c889145348c0208fec5aa754383
O'Brian Santos <obrian.santos@gmail.com>
2022-08-20 11:14:11 -0700

    Remove amplify.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { LoginIcon } from '@heroicons/react/solid'
 import './App.css';
 
 function App() {
@@ -10,12 +11,26 @@ function App() {
 
   return (
     <div className="container flex flex-col mx-auto min-h-screen bg-lime-100">
+      
+      <div className='flex flex-row justify-end'>
+        <div className='hidden m-4 flex-row justify-center items-center bg-lime-600 rounded-2xl w-36 h-8'>
+          <a className='text-lime-50' href='https://memesave.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=2si0r1d1pfqai8t91fl1hpd62i&redirect_uri=https://localhost:3000'>sign in</a>
+        </div>
+        <div className='m-4'>
+          <a className='text-lime-50' href='https://memesave.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=2si0r1d1pfqai8t91fl1hpd62i&redirect_uri=https://localhost:3000'>
+            <LoginIcon className='bg-lime-600 text-lime-50 rounded-lg w-16 h-8'></LoginIcon>
+          </a>
+        </div>
+      </div>
+      
+      
       <div className='flex m-6 flex-col h-48 justify-evenly items-center'>
         <div className='text-lg text-lime-600 text-center font-semibold'> Look up your saved memes to send to friends!</div>
         <input onChange={(e) => getSuggestions(e)} className='p-2 rounded-md w-48 h-6'></input>
-
-        {/* <button onClick={() => console.log(search)} className='text-lime-50 bg-lime-600 rounded-2xl w-36 h-8'>Search</button> */}
       </div>
+        
+
+      
     </div>
   );
 }
