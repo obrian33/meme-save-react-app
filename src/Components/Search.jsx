3c787e26f47bc73361f7fbc1b750b4f88369b00a
obrian33 <obrians@uci.edu>
2022-08-30 17:47:52 -0700

    Set search results

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+import { GetMemeSuggestions } from "../api/SearchAPICalls/GetCalls";
+import { getUserToken } from "../utilities/TokenHandling";
+import jwt_decode from 'jwt-decode';
+
+const SearchResult = ({ imgsrc, searchPhrase }) => {
+    return <img src={`https://meme-save.s3.us-west-2.amazonaws.com/${imgsrc}`} alt={searchPhrase} width='100px' height='100px'></img>
+}
 
 const Search = () => {
+    const [phrase, setPhrase] = useState('');
+    const [searchSuggestions, setSearchSuggestions] = useState([]);
+
     const getSuggestions = (event) => {
-        console.log(event.target.value);
+        setPhrase(event.target.value);
+        if(event.target.value !== '')  {
+            getMemes(event.target.value);
+        } else {
+            setSearchSuggestions([]);
+        }
+        
+
     }
 
+    const getMemes = async (phrase) => {
+        let parsedToken = getUserToken();
+        let decodedToken = jwt_decode(parsedToken.id_token);
+        let memeResponse = await GetMemeSuggestions(parsedToken.id_token, phrase, decodedToken.email)
+
+        let memeResults = await memeResponse.json();
+
+        console.log(memeResults);
+
+        setSearchSuggestions(memeResults.Items);
+
+    }
     
  
     return (
@@ -13,6 +43,11 @@ const Search = () => {
                 <div className='flex flex-col justify-evenly items-center h-40'>
                     <div className='text-lg text-lime-600 text-center font-semibold'> Look up your saved memes to send to friends!</div>
                     <input onChange={(e) => getSuggestions(e)} className='p-2 rounded-md w-48 h-6'></input>
+                    <div className="z-10">
+                        {searchSuggestions.map((searchSuggestion) => {
+                            return <SearchResult key={searchSuggestion.memekey} imgsrc={searchSuggestion.s3key} searchPhrase={phrase}></SearchResult>
+                        })}
+                    </div>
                 </div>
             </div>
         </>
