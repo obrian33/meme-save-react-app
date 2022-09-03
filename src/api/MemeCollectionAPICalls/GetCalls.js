15f0256b06c91ce9ba447c1018801c735fbc6624
obrian33 <obrians@uci.edu>
2022-09-02 18:00:48 -0700

    Added new get call to get memes per group

--- src/api/MemeCollectionAPICalls/GetCalls.js
+++ src/api/MemeCollectionAPICalls/GetCalls.js
@@ -7,3 +7,13 @@ export const getMemeGroups = async (idtoken, user) => {
         }
     });
 }
+
+export const getMemesPerGroup = async (idtoken, user, group) => {
+    return fetch(`https://obv030u051.execute-api.us-west-2.amazonaws.com/prod/getmemecollection?user=${user}&memegroup=${group}`, {
+        method: 'GET',
+        mode : 'cors',
+        headers : {
+            'Authorization' : idtoken,
+        }
+    });
+}
