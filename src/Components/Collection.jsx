7f78ffb67c1dbceb37d36b5b96e2705cae874a53
obrian33 <obrians@uci.edu>
2022-09-02 18:02:26 -0700

    Showing memes by group

--- src/Components/Collection.jsx
+++ src/Components/Collection.jsx
@@ -1,16 +1,22 @@
 import { Fragment, useEffect, useState } from "react";
 import { getUserToken } from '../utilities/TokenHandling'
-import { getMemeGroups } from "../api/MemeCollectionAPICalls/GetCalls";
+import { getMemeGroups, getMemesPerGroup } from "../api/MemeCollectionAPICalls/GetCalls";
 import { Listbox, Transition } from "@headlessui/react";
 import jwt_decode from 'jwt-decode';
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 
+const Meme = ({src}) => {
+    return (
+        <img className="w-28 m-2" src={`https://meme-save.s3.us-west-2.amazonaws.com/${src}`} alt='3'></img>
+    );
+}
+
 const Collection = () => {
     const [memeGroups, setMemeGroups] = useState([]);
     const [selected, setSelected] = useState('');
+    const [memes, setMemes] = useState([]);
 
     useEffect(() => {
-
         
         let getGroups = async () => {
             let parsedToken = getUserToken();
@@ -22,16 +28,34 @@ const Collection = () => {
             
             setMemeGroups(memeGroups);
             setSelected(memeGroups[0]);
+
+            let memesByGroupsResponse = await getMemesPerGroup(parsedToken.id_token, decodedIDToken.email, memeGroups[0]);
+            let memesByGroup = await memesByGroupsResponse.json();
+
+            setMemes(memesByGroup.Items);
         };
 
         getGroups();
-    }, [])
+    }, []);
+
+    let getMemesBySelectedGroup = async (selectedGroup) => {
+        setSelected(selectedGroup);
+
+        let parsedToken = getUserToken();
+            
+        let decodedIDToken = jwt_decode(parsedToken.id_token);
+
+        let getMemesPerGroupResponse = await getMemesPerGroup(parsedToken.id_token, decodedIDToken.email, selectedGroup);
+        let memesPerGroup = await getMemesPerGroupResponse.json();
+        
+        setMemes(memesPerGroup.Items);
+    }
     
     return (
         <div className="flex justify-center">
             <div className="flex items-center flex-col">
                 <div className="my-4 text-lime-700 text-3xl">Meme Groups</div>
-                <Listbox value={selected} onChange={setSelected}>
+                <Listbox value={selected} onChange={(e) => getMemesBySelectedGroup(e)}>
                     <div className="relative mt-1">
                     <Listbox.Button className="h-9 relative w-72 cursor-default rounded-lg bg-lime-50 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-lime-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-lime-300 sm:text-sm">
                         <span className="block truncate">{selected}</span>
@@ -81,6 +105,11 @@ const Collection = () => {
                     </Transition>
                     </div>
                 </Listbox>
+                <div className="flex justify-center overflow-y-auto flex-wrap mt-5 h-48">
+                {memes.map((x, idx) => {
+                    return <Meme key={idx} src={x.s3key}></Meme>
+                } )}
+                </div>
             </div>
         </div>
     );
