bac16e2b22d82a437e4b0f2eded5b77a0a374f25
obrian33 <obrians@uci.edu>
2022-10-15 11:29:08 -0700

    add spinner, valid checkmark, and invalid mark

--- src/Components/AddMeme.jsx
+++ src/Components/AddMeme.jsx
@@ -8,16 +8,23 @@ import { v4 as uuidv4 } from 'uuid';
 import { checkIfMemeKeyIsUnique } from '../api/AddMemeAPICalls/GetCalls';
 import { useForm } from "react-hook-form";
 import _ from 'lodash';
+import Spinner from './Spinner';
+import { BadgeCheckIcon, XCircleIcon } from '@heroicons/react/solid'
 
 const AddMeme = () => {
     const [isSuccessful, setSuccessfullyAdded] = useState(false);
-    const [isCheckingMemeKey, setCheckingMemeKey] = useState(true);
+    const [isCheckingMemeKey, setCheckingMemeKey] = useState(false);
     const [modalText, setModalText] = useState('');
     const [modalTitle, setModalTitle] = useState('');
     const [hasRetried, setHasRetried] = useState(false);
     const [hasRetriedDynamoDB, setHasRetriedDynamoDB] = useState(false);
-    const { register, handleSubmit, trigger, setValue, formState: { errors } } = useForm({ mode: 'onSubmit', reValidateMode: 'onSubmit'});
+    const { register, handleSubmit, trigger, setValue, formState : { errors, isValidating }, getFieldState } = useForm({ mode: 'onSubmit', reValidateMode: 'onSubmit', defaultValues: {
+        memekey: '',
+        memegroup: '',
+        file: {}
+    }});
 
+    
     const createFileName = (imageFile) => {
         return `${uuidv4()}.${getFileTypeExtension(imageFile)}`;
     }
@@ -151,22 +158,36 @@ const AddMeme = () => {
                         </div>
                     </div>
 
-                    <div className='flex flex-row justify-between w-72'>
+                    <div className='flex flex-row items-center w-72'>
                         <div className="text-sm mr-1 after:content-['*'] after:ml-0.5 after:text-red-500 ">Meme Key:</div>
                         <div className='flex flex-col'>
-                        <div className='flex'>
-                        <input {...register('memekey', { validate: async (value) => { 
+                        <div className='flex items-center'>
+                        <input {...register('memekey', {
+                            validate: async (value) => { 
 
                             if(value === '') {
-                                return false;
+                                return "can't be empty";
                             }
-                            let token = getUserToken();
-                            let decodedIdToken = jwt_decode(token.id_token);
+                            // let token = getUserToken();
+                            // let decodedIdToken = jwt_decode(token.id_token);
+                            
                             setCheckingMemeKey(true);
+
+                            let d = () => { return new Promise(resolve => {
+                                setTimeout(() => {
+                                    resolve('resolved');
+                                }, 3000)
+                            })};
+
+                            console.log('before promise');
+                            let res = await d();
+
+                            console.log(res)
                             // let res = await checkIfMemeKeyIsUnique(token.id_token, value, decodedIdToken.email);
                             // let json = await res.json();
-                            // setCheckingMemeKey(false);
-                            return false
+                            
+                            setCheckingMemeKey(false);
+                            
                             // return json.Items.length === 0;
                             } 
                         }) } onChange={_.debounce((e) => {
@@ -174,17 +195,17 @@ const AddMeme = () => {
                                 trigger('memekey');
                             }, 1000)
                         } placeholder='Key must be unique!' className='text-sm p-2 rounded-md h-6'></input>
-                            { isCheckingMemeKey &&     <svg aria-hidden="true" class="w-5 h-8 text-lime-400 animate-spin fill-lime-600" viewBox="0 0 100 101">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    </svg>}
 
+                            { isCheckingMemeKey && <Spinner/>}
+                            { !errors.memekey && !isValidating && <BadgeCheckIcon className='w-4 h-4 ml-2 text-lime-700'/>}
+                            { errors.memekey && <XCircleIcon className='w-4 h-4 ml-2 text-red-700'/>}
+                            
                         </div>
-                        {errors.memekey && <span>This field is required</span>}
+                        {errors.memekey && <span>{errors.memekey.message}</span>}
                         </div>
                     </div>
 
-                    <div className='flex flex-row justify-between w-72'>
+                    <div className='flex flex-row w-72'>
                         <div className='text-sm mr-1'>Meme Group:</div>
                         <input {...register('memegroup')} placeholder='Select a group!' className='text-sm p-2 rounded-md h-6'></input>
                     </div>
